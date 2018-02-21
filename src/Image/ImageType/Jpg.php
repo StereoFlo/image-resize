231a979040cea4e5f9a1afff81ad38da7ed4da71
StereoFlo <StereoFlo@users.noreply.github.com>
2018-02-21 18:25:29 +0300

    Update Jpg.php

--- src/Image/ImageType/Jpg.php
+++ src/Image/ImageType/Jpg.php
@@ -26,7 +26,7 @@ class Jpg extends AbstractImage
         if ($toBrowser) {
             $this->setHeader($this->getImageInfo()->getMimeType());
             if (empty($this->resizedImage)) {
-                return \imagejpeg($this->image, null, $this->compression);
+                return \readfile($this->image, null, $this->compression);
             }
             return \imagejpeg($this->resizedImage, null, $this->compression);
         }
