01df68b07e85960a4f08fe8d09b55fde06129de4
StereoFlo <StereoFlo@users.noreply.github.com>
2018-02-21 18:26:13 +0300

    Update Wbmp.php

--- src/Image/ImageType/Wbmp.php
+++ src/Image/ImageType/Wbmp.php
@@ -26,7 +26,7 @@ class Wbmp extends AbstractImage
         if ($toBrowser) {
             $this->setHeader($this->getImageInfo()->getMimeType());
             if (empty($this->resizedImage)) {
-                return \imagejpeg($this->image, null, $this->compression);
+                return \readfile($this->image, null, $this->compression);
             }
             return \imagewbmp($this->resizedImage);
         }
